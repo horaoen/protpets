2300103fc798f31e758a8bbd7919c43c12163795
叶计义 <horaoen@gmail.com>
2023-05-18 13:47:25 +0800

    init backend

---
+++ backend/protpets/src/main/java/com/horaoen/protpets/ProtpetsApplication.java
@@ -0,0 +1,13 @@
+package com.horaoen.protpets;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProtpetsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProtpetsApplication.class, args);
+    }
+
+}
---
+++ backend/protpets/src/test/java/com/horaoen/protpets/ProtpetsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.horaoen.protpets;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProtpetsApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
