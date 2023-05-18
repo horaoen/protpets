e5ddf47e303d0795c3b0eb1270a848f3edae9587
horaoen <horaoen@gmail.com>
2023-05-18 17:41:22 +0800

    配置CommandlineRunner和Swagger

--- backend/protpets/src/main/java/com/horaoen/protpets/ProtpetsApplication.java
+++ backend/protpets/src/main/java/com/horaoen/protpets/ProtpetsApplication.java
@@ -1,13 +1,29 @@
 package com.horaoen.protpets;
 
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-@SpringBootApplication
-public class ProtpetsApplication {
+import java.net.InetAddress;
 
+@SpringBootApplication
+@Slf4j
+public class ProtpetsApplication implements CommandLineRunner{
+    @Value("${server.port}")
+    private String port;
     public static void main(String[] args) {
         SpringApplication.run(ProtpetsApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+        String ip = InetAddress.getLocalHost().getHostAddress();
+        log.info("=========================================================");
+        log.info("local：http://localhost:{}", port);
+        log.info("external：http://{}:{}", ip, port);
+        log.info("swagger：http://{}:{}/doc.html", ip, port);
+        log.info("=========================================================");
+    }
 }
